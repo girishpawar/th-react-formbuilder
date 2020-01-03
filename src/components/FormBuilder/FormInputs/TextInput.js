a7be5a9b7ee8bf09f8c43fd20840443eb7fa4f98
Girish Pawar <girish.suresh.pawar@gmail.com>
2020-01-03 13:29:50 +0530

    Adding script form creating build of package

---
+++ src/components/FormBuilder/FormInputs/TextInput.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import HeaderLabel from "./HeaderLabel";
+
+class TextInput extends Component {
+  render() {
+    
+    const  {
+      type,
+      meta,
+      label,
+      item,
+      input,
+      readOnly,
+      required,
+      generator,
+      className,
+      showError,
+      defaultValue,
+    } = this.props;
+
+    const _props = generator ? {
+      ...input,
+      disabled: readOnly,
+      value: defaultValue || input.value,
+      onChange: e => input.onChange(e.target.value),
+      style: {
+        borderColor: meta.touched && required && meta.error ? "red" : ""
+      },
+    } : {}
+    
+    return (
+      <div>
+        <HeaderLabel
+          label={generator ? label : item.label}
+          required={generator ? required : item.required}
+          readOnly={readOnly}
+        />
+        <input 
+          {..._props}
+          type={type}
+          className={className} 
+        />
+        {generator ? showError(meta.touched, meta.error, meta.warning) : ''}
+      </div>
+    );
+  }
+}
+
+TextInput.defaultProps = {
+  generator: false,
+  className: "form-control"
+}
+
+export default TextInput;
